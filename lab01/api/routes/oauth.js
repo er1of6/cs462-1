7ddba461076f22ce6f07c10e78181453129eb01c
Jason Rasmussen <jrasm91@gmail.com>
2014-02-06 13:39:00 -0700

    fixed bug

--- lab01/api/routes/oauth.js
+++ lab01/api/routes/oauth.js
@@ -17,10 +17,23 @@ passport.deserializeUser(function(id, done) {
 passport.use(new FoursquareStrategy({
 		clientID: FOURSQUARE_CLIENT_ID,
 		clientSecret: FOURSQUARE_CLIENT_SECRET,
-		callbackURL: "http://" + "/api/login/callback"
+		callbackURL: "http://localhost:5000/api/login/callback"
 	},
 	function(accessToken, refreshToken, profile, done) {
-		User.findOrCreate(profile._json.response.user, done);
-		
+		var request = require('request');
+		var url = 'https://api.foursquare.com/v2/users/self/checkins?oauth_token=' + accessToken + '&v=20130205'
+
+		request(url, function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var newInfo = JSON.parse(body).response.checkins;
+				console.log('new info', JSON.stringify(profile, null, 4));
+				profile._json.response.user.checkins = newInfo;
+				User.findOrCreate(profile._json.response.user, done);
+			}
+			else {
+				done('error getting checkin data');
+			}
+		})
+
 	}
 ));
